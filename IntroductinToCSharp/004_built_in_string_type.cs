9b2539023402a381785a4f39e37314abfe298b13
robertmirro <rmirro@gmail.com>
2014-10-25 20:00:11 -0400

    built-in string, escape chars and verbatim literal

---
+++ IntroductinToCSharp/004_built_in_string_type.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class built_in_string_type_004
+{
+    static void Main()
+    {
+        // escape quotes, new line
+        string Name = "El \"mirro\" Bobbio \nis\nkinda\nkewl";
+        Console.WriteLine(Name);
+
+        // escape backslash (file paths), bell
+        string filename = "c:\\myfolder\\mysubfolder\\myfile \a \a \a";
+        Console.WriteLine(filename);
+
+        // verbatim literal (treat escape sequences as printable characters    
+        string filename2 = @"c:\myfolder\mysubfolder\myfile \a \a";
+        Console.WriteLine(filename2);
+    }
+}
